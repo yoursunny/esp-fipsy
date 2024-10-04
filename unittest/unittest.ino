0e2557cf6b19e08b482a91c97b5aaaf43a502c87
Matthew Zamora <matt@mocomakers.com>
2024-10-04 19:36:51 -0400

    Fix length of L for MachXO2

--- unittest/unittest.ino
+++ unittest/unittest.ino
@@ -5,6 +5,7 @@
 #include <EpoxyFS.h>
 
 test(ParseV1) {
+  // For FispyV1 - MachXO2-256
   fipsy::FuseTable fuseTable;
   fuseTable.resize(73600);
 
@@ -17,6 +18,22 @@ test(ParseV1) {
   assertEqual(jc.features.featureRow(), static_cast<uint64_t>(0));
   assertEqual(jc.features.feabits(), 0x0420);
   assertEqual(fuseTable.computeChecksum(), 0xA0A5);
+}
+
+test(ParseV2) {
+  // For FispyV2 - MachXO2-1200
+  fipsy::FuseTable fuseTable;
+  fuseTable.resize(343936);
+
+  auto file = fs::EpoxyFS.open("v2-blinky1.jed", "r");
+  auto jc = fipsy::parseJedec(file, fuseTable);
+  file.close();
+
+  assertEqual(static_cast<int>(jc.error), 0);
+  assertTrue(static_cast<bool>(jc));
+  assertEqual(jc.features.featureRow(), static_cast<uint64_t>(0));
+  // assertEqual(jc.features.feabits(), 0x0420);
+  assertEqual(fuseTable.computeChecksum(), 0x99AE);
 }
 
 void
